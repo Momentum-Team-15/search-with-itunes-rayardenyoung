1fd43dbc2e092e8f1ecea14d0d1f7790c6abe829
Ray Young <ray.arden.young@gmail.com>
2022-09-28 13:39:16 -0400

    search works, clear html code in place

--- main.js
+++ main.js
@@ -1,45 +1,73 @@
 const musicPlayerContainer = document.querySelector("#music-player-container");
 
-const url = 'https://proxy-itunes-api.glitch.me/search?term=jack+johnson'
+
+let input = document.getElementById("music-search");
+let form = document.getElementById("form");
+let searchResults = document.getElementById("search-results");
+//let clearButton = document.getElementById('clear');
+
+
+
+// function clearResults() {
+//     if (myData.results === true) {
+//         console.log ("this is true");
+//       searchResults = ""
+//     }
+//   }
+//   clearButton.addEventListener("click", (event) => {
+//       clearResults()
+//   })
+  
+
+
+
+form.addEventListener("submit", (event) => {
+    let search = input.value;
+    let url = `https://itunes.apple.com/search?term=${search}&limit=25`
+    event.preventDefault();
 
 fetch(url, {
     method: 'GET',
     headers: {'Content-Type': 'application/json'},
 })
-
-.then(function(response){
-    return response.json()
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (myData) {
+        searchResults.innerHTML = ""
+        buildResults(myData.results);
+    })
 })
 
-.then(function(myData){
+
+    function buildResults(itunesArray) {
+            for (let data of itunesArray) {
+                //console.log(`${data.trackName} by ${data.artistName}`)
+
+                let resultsDiv = document.createElement("div");
+                let resultsSongPic = document.createElement("img");
+                let resultsArtistName = document.createElement("h2");
+                let resultsSongName = document.createElement("p");
+                let resultsAlbumName = document.createElement("p");
+                let resultsPlaySong = document.getElementById("audio");
+                // let resultsPreviewSong = document.createElement ("audio");
+
+                resultsSongPic.src = `${data.artworkUrl60}`;
+                resultsArtistName.innerText = `${data.artistName}`;
+                resultsSongName.innerText = `${data.trackName}`;
+                resultsAlbumName.innerText = `${data.collectionName}`;
+                resultsPlaySong.src = `${data.trackViewUrl}`;
+                // resultsPreviewSong.src = `${data.previewUrl}`;
+
+                resultsDiv.appendChild(resultsSongPic);
+                resultsDiv.appendChild(resultsArtistName);
+                resultsDiv.appendChild(resultsSongName);
+                resultsDiv.appendChild(resultsAlbumName);
+                // resultsDiv.appendChild(resultsPlaySong);
+                // customerDiv.appendChild(resultsPreviewSong);
+                searchResults.appendChild(resultsDiv);
+
+                resultsDiv.classList.add("results-div");
+            }
+            }
     
-    for (let data of myData.results){
-        console.log(`${data.trackName} by ${data.artistName}`)
-
-        
-        let resultsDiv = document.createElement("div");
-        let resultsSongPic = document.createElement("img");
-        let resultsArtistName = document.createElement("h2");
-        let resultsSongName = document.createElement("p");
-        let resultsAlbumName = document.createElement("p");
-        let resultsPlaySong = document.createElement("audio");
-        // let resultsPreviewSong = document.createElement ("audio");
-
-        resultsSongPic.src = `${data.artworkUrl60}`;
-        resultsArtistName.innerText = `${data.artistName}`;
-        resultsSongName.innerText = `${data.trackName}`;
-        resultsAlbumName.innerText = `${data.collectionName}`;
-        resultsPlaySong.src = `${data.trackViewUrl}`;
-        // resultsPreviewSong.src = `${data.previewUrl}`;
-
-        resultsDiv.appendChild(resultsSongPic);
-        resultsDiv.appendChild(resultsArtistName);
-        resultsDiv.appendChild(resultsSongName);
-        resultsDiv.appendChild(resultsAlbumName);
-        resultsDiv.appendChild(resultsPlaySong);
-        // customerDiv.appendChild(resultsPreviewSong);
-        musicPlayerContainer.appendChild(resultsDiv);
-
-        resultsDiv.classList.add("results-box");
-
-   }})
